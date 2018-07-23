bdfa77fc50cc6c5d1c2c14a9d82b58000a1106c6
北辰青 <>
2018-07-24 01:06:46 +0800

    mmm

---
+++ Core_Foundation/Core_Foundation/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Core_Foundation
+//
+//  Created by 狮博 on 2017/8/24.
+//  Copyright © 2017年 狮博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+
+@end
+
---
+++ Core_Foundation/Core_Foundation/ViewController.m
@@ -0,0 +1,69 @@
+//
+//  ViewController.m
+//  Core_Foundation
+//
+//  Created by 狮博 on 2017/8/24.
+//  Copyright © 2017年 狮博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonA:(id)sender {
+    
+}
+
+
+- (IBAction)buttonB:(id)sender {
+    
+}
+
+- (IBAction)buttonC:(id)sender {
+    
+}
+
+- (IBAction)buttonD:(id)sender {
+    
+}
+
+- (IBAction)buttonE:(id)sender {
+    
+}
+
+
+- (IBAction)buttonF:(id)sender {
+    
+}
+
+- (IBAction)ButtonG:(id)sender {
+    
+}
+
+- (IBAction)buttonH:(id)sender {
+    
+}
+
+- (IBAction)buttonI:(id)sender {
+    
+}
+
+- (IBAction)buttonJ:(id)sender {
+    
+}
+
+@end
